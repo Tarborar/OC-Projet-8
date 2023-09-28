c613c8b546d8671a1e99e4fcb736740b757dab78
Tarborar <mathieuseurre84@gmail.com>
2023-09-28 17:27:29 +0200

    style skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,61 +1,42 @@
 import { useState } from 'react';
 import data from '../datas/skills.json';
 
-import '../styles/Skills.scss'
+import '../styles/Skills.scss';
 import Button from '../components/Button';
 
-function Skills(){
-
-    const [toggleState, setToggleState] = useState(1);
+function Skills() {
+    const [toggleState, setToggleState] = useState(0);
 
     const toggleTab = (index) => {
         setToggleState(index);
     }
 
-    const currentData = data[toggleState - 1];
-
-    return(
+    return (
         <div className='skills-bloc'>
-            <div className={toggleState === 1 ? 'skills-bloc__information--active' : 'skills-bloc__information'}>
-                <h2 className='titleText'>{currentData.title}</h2>
-                <ul>
-                    {currentData.technologies.map((tech, index) => (
-                        <li key={index}>{tech.name}</li>
-                    ))}
-                </ul>
-                <h3 className='subtitleText montserrat'>{currentData.subtitle}</h3>
-                <p className='middleText montserrat'>{currentData.paragraph}</p>
-                <Button cta={'Projets'} />
-            </div>
-            <div className={toggleState === 2 ? 'skills-bloc__information--active' : 'skills-bloc__information'}>
-                <h2 className='titleText'>{currentData.title}</h2>
-                <ul>
-                    {currentData.technologies.map((tech, index) => (
-                        <li key={index}>{tech.name}</li>
-                    ))}
-                </ul>
-                <h3 className='subtitleText montserrat'>{currentData.subtitle}</h3>
-                <p className='middleText montserrat'>{currentData.paragraph}</p>
-                <Button cta={'Projets'} />
-            </div>
-            <div className={toggleState === 3 ? 'skills-bloc__information--active' : 'skills-bloc__information'}>
-                <h2 className='titleText'>{currentData.title}</h2>
-                <ul>
-                    {currentData.technologies.map((tech, index) => (
-                        <li key={index}>{tech.name}</li>
-                    ))}
-                </ul>
-                <h3 className='subtitleText montserrat'>{currentData.subtitle}</h3>
-                <p className='middleText montserrat'>{currentData.paragraph}</p>
-                <Button cta={'Projets'} />
-            </div>
+            {data.map((item, index) => (
+                <div key={index} className={toggleState === index ? 'skills-bloc__information skill-active' : 'skills-bloc__information'}>
+                    <h2 className='titleText'>{item.title}</h2>
+                    <ul>
+                        {item.technologies.map((tech, techIndex) => (
+                            <li key={techIndex}>{tech.name}</li>
+                        ))}
+                    </ul>
+                    <h3 className='subtitleText montserrat'>{item.subtitle}</h3>
+                    <p className='middleText montserrat'>{item.paragraph}</p>
+                    <Button cta={'Projets'} />
+                </div>
+            ))}
+            
+            <div className='skills-bloc__line'></div>
             <nav className='skills-bloc__navigation'>
-                <div className={toggleState === 1 ? 'mediumText skills-bloc__navigation--active' : 'mediumText'} onClick={() => toggleTab(1)}>Compétence 1</div>
-                <div className={toggleState === 2 ? 'mediumText skills-bloc__navigation--active' : 'mediumText'} onClick={() => toggleTab(2)}>Compétence 2</div>
-                <div className={toggleState === 3 ? 'mediumText skills-bloc__navigation--active' : 'mediumText'} onClick={() => toggleTab(3)}>Compétence 3</div>
+                {data.map((item, index) => (
+                    <div key={index} className={toggleState === index ? 'mediumText skills-bloc__navigation--btn skill-btn-active' : 'mediumText skills-bloc__navigation--btn'} onClick={() => toggleTab(index)}>
+                        {item.title}
+                    </div>
+                ))}
             </nav>
         </div>
     );
 }
 
-export default Skills
+export default Skills;
