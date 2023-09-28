ff75abbfaf3761abd612833935177f3d864610aa
Tarborar <mathieuseurre84@gmail.com>
2023-09-28 15:39:21 +0200

    skills tabs & datajson

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import '../styles/About.scss'
 import Button from '../components/Button'
 
@@ -7,7 +9,7 @@ function About(){
             <div className="about-bloc__information">
                 <h2 className='titleText'>Qui suis-je ?</h2>
                 <h3 className='subtitleText montserrat'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, placeat.</h3>
-                <Button cta={'En savoir plus'}/>
+                <Link to="/skills"><Button cta={'En savoir plus'}/></Link>
                 <p className='mediumText montserrat'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi amet libero, praesentium quidem, neque ullam earum laborum at qui itaque quas eum, aspernatur facere dolorum repellendus animi perspiciatis consequatur debitis.
                 Expedita, commodi? Perferendis nulla nam impedit? Corporis deserunt doloribus sequi, ut libero, beatae in, voluptas unde nesciunt maxime dolor!</p>
             </div>
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,41 +1,59 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import data from '../datas/skills.json';
 
 import '../styles/Skills.scss'
 import Button from '../components/Button';
 
 function Skills(){
+
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
+    const currentData = data[toggleState - 1];
+
     return(
         <div className='skills-bloc'>
-            <div className='skills-bloc__information'>
-                <h2 className='titleText'>Compétence 1</h2>
-                <div>
-                    <ul>
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                    </ul>
-                </div> 
-                <h3 className='subtitleText montserrat'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, placeat.</h3>
-                <p className='middleText montserrat'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi amet libero, praesentium quidem, neque ullam earum laborum at qui itaque quas eum, aspernatur facere dolorum repellendus animi perspiciatis consequatur debitis.
-                    Expedita, commodi? Perferendis nulla nam impedit? Corporis deserunt doloribus sequi, ut libero, beatae in, voluptas unde nesciunt maxime dolor!</p>
-                <Button cta={'Projets'}/>
+            <div className={toggleState === 1 ? 'skills-bloc__information--active' : 'skills-bloc__information'}>
+                <h2 className='titleText'>{currentData.title}</h2>
+                <ul>
+                    {currentData.technologies.map((tech, index) => (
+                        <li key={index}>{tech.name}</li>
+                    ))}
+                </ul>
+                <h3 className='subtitleText montserrat'>{currentData.subtitle}</h3>
+                <p className='middleText montserrat'>{currentData.paragraph}</p>
+                <Button cta={'Projets'} />
             </div>
-            <div className='skills-bloc__navigation'>
-                <nav>
-                    <Link to="./skills" className='noLinkDecoration'>
-                        <img src="" alt="" />
-                        <div className='mediumText'>Compétence 1</div>
-                    </Link>
-                    <Link to="./skills" className='noLinkDecoration'>
-                        <img src="" alt="" />
-                        <div className='mediumText'>Compétence 2</div>
-                    </Link>
-                    <Link to="./skills" className='noLinkDecoration'>
-                        <img src="" alt="" />
-                        <div className='mediumText'>Compétence 3</div>
-                    </Link>
-                </nav>
+            <div className={toggleState === 2 ? 'skills-bloc__information--active' : 'skills-bloc__information'}>
+                <h2 className='titleText'>{currentData.title}</h2>
+                <ul>
+                    {currentData.technologies.map((tech, index) => (
+                        <li key={index}>{tech.name}</li>
+                    ))}
+                </ul>
+                <h3 className='subtitleText montserrat'>{currentData.subtitle}</h3>
+                <p className='middleText montserrat'>{currentData.paragraph}</p>
+                <Button cta={'Projets'} />
             </div>
+            <div className={toggleState === 3 ? 'skills-bloc__information--active' : 'skills-bloc__information'}>
+                <h2 className='titleText'>{currentData.title}</h2>
+                <ul>
+                    {currentData.technologies.map((tech, index) => (
+                        <li key={index}>{tech.name}</li>
+                    ))}
+                </ul>
+                <h3 className='subtitleText montserrat'>{currentData.subtitle}</h3>
+                <p className='middleText montserrat'>{currentData.paragraph}</p>
+                <Button cta={'Projets'} />
+            </div>
+            <nav className='skills-bloc__navigation'>
+                <div className={toggleState === 1 ? 'mediumText skills-bloc__navigation--active' : 'mediumText'} onClick={() => toggleTab(1)}>Compétence 1</div>
+                <div className={toggleState === 2 ? 'mediumText skills-bloc__navigation--active' : 'mediumText'} onClick={() => toggleTab(2)}>Compétence 2</div>
+                <div className={toggleState === 3 ? 'mediumText skills-bloc__navigation--active' : 'mediumText'} onClick={() => toggleTab(3)}>Compétence 3</div>
+            </nav>
         </div>
     );
 }
