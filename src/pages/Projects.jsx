828388aa1a486cb48afe2c08c2bffe072ddf389c
Tarborar <mathieuseurre84@gmail.com>
2023-09-29 09:00:38 +0200

    navigation project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,18 +1,31 @@
+import { useState } from 'react';
+
 import '../styles/Projects.scss'
 import Card from '../components/Card'
 
 function Projects(){
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
     return(
         <div className='projects-page'>
             <h2 className='titleText'>Projets</h2>
             <div className='projects-bloc'>
-                <div className='projects-bloc__content'> 
-                    <div className='projects-bloc__content--left-line'>Projet 1</div>
-                    <div className='projects-bloc__content--main-line'><Card/></div> 
-                    <div className='projects-bloc__content--right-line'>Projet 1</div>
+                <div className={toggleState === 1 ? 'projects-bloc__web active-web' : 'projects-bloc__web'}> 
+                    <div className='projects-bloc__web--left-line'>Projet 1</div>
+                    <div className='projects-bloc__web--main-line'><Card/></div> 
+                    <div className='projects-bloc__web--right-line'>Projet 1</div>
                 </div> 
+                <div className={toggleState === 2 ? 'projects-bloc__logo active-logo' : 'projects-bloc__logo'}>
+                    Logo
+                </div>
                 <div className='projects-bloc__navigation'>
-                    navigation
+                    <div className={toggleState === 1 ? 'projects-bloc__navigation__btn active' : 'projects-bloc__navigation__btn'} onClick={() => toggleTab(1)}><div className='smallText projects-bloc__navigation__btn--text'>WEB</div></div>
+                    <div className={toggleState === 2 ? 'projects-bloc__navigation__btn active' : 'projects-bloc__navigation__btn'} onClick={() => toggleTab(2)}><div className='smallText projects-bloc__navigation__btn--text'>LOGO</div></div>
+                    <div className={toggleState === 3 ? 'projects-bloc__navigation__btn active' : 'projects-bloc__navigation__btn'} onClick={() => toggleTab(3)}><div className='smallText projects-bloc__navigation__btn--text'>?</div></div>
                 </div>    
             </div>
         </div>
