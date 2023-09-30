78c460cc9ada8cb444a66a8c1dc304cb1b5b29c1
Tarborar <mathieuseurre84@gmail.com>
2023-09-30 20:38:05 +0200

    Texte about/skills

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,11 +7,13 @@ function About(){
     return(
         <div className='about-bloc'>
             <div className="about-bloc__information">
-                <h2 className='titleText'>Qui suis-je ?</h2>
-                <h3 className='subtitleText montserrat'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, placeat.</h3>
+                <h2 className='titleText'>À propos</h2>
+                <h3 className='subtitleText montserrat'>Mathieu Seurre, Développeur front-end & Graphiste passionné</h3>
                 <Link to="/skills" className='button-link'><Button cta={'En savoir plus'}/></Link>
-                <p className='mediumText montserrat'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi amet libero, praesentium quidem, neque ullam earum laborum at qui itaque quas eum, aspernatur facere dolorum repellendus animi perspiciatis consequatur debitis.
-                Expedita, commodi? Perferendis nulla nam impedit? Corporis deserunt doloribus sequi, ut libero, beatae in, voluptas unde nesciunt maxime dolor!</p>
+                <p className='smallText montserrat'>En développant une application en C# pour le projet de fin d'année pour compléter ma seconde année d'études de BTS, je découvre un intérêt Gigantesque pour l'UI/UX design. 
+                Je conçois l'interface graphique de l'application PC, mobile et Web pour tout mon groupe de projet. <br /><br /> 
+                Je me suis d'abord intéressé à la conception de logos puis j'ai commencé à approfondir mes connaissances en autodidacte pour devenir développeur front-end.
+                <br /><br />C’est alors que je me suis inscrit à la formation d’Openclassrooms pour pousser mon niveau au plus haut et ainsi obtenir la liberté de création que j'ai aujoud'hui.</p>
             </div>
             <div className='about-bloc__image'><div className='about-bloc__image--test'></div></div>
         </div>
